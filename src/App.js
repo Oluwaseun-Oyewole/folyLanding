d7d14dbaafa9877209805b065b86451172c9e2db
Oluwaseun-Oyewole <findseunoyewole@gmail.com>
2022-03-22 22:59:02 +0100

    Design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, Purpose } from "./components";
+import { Header } from "./container";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Navbar />
+        <Header />
+        <Purpose />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Navbar } from "./navbar/Navbar.jsx";
+export { default as Purpose } from "./purpose/Purpose.jsx";
